d1c5c8612f894e2f0bd4536448d10b5767cd276f
antimlgpro <ludvigvonreis@gmail.com>
2018-11-06 21:16:59 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,68 @@
+var numclicks = 0;
+var delay = 500;
+var multi = 5;
+
+
+//player stuff
+var items = [];
+var clicks = 0;
+
+function clickbtn() {
+    addclick();
+}
+
+function addclick() {
+    numclicks++;
+}
+
+function autoclick() {
+    window.setInterval(function() {
+        addclick();
+    }, delay);
+}
+
+function buyItem(item, amount) {
+    if(numclicks >= 100) {
+        numclicks = numclicks - 100;
+        if(item == "autoclicker") {
+            items.push(item);
+            item = "";
+        }
+    }
+    else 
+    {
+     document.getElementById("warning").innerHTML = "You do not have enough clicks.";
+     setTimeout(function(){ 
+     for(var i = 0; i <= 100; i = i + 10) {
+     document.getElementById("warning").style.opacity =- i;
+     }
+     }, 2000);
+    }
+}
+
+function sleep(delay2) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay2);
+      }
+
+function gameLoop() {
+    window.setInterval(function(){
+    items.forEach(function(value){
+        if(value == "autoclicker") {
+            if(clicks < items.length) {
+            clicks++;
+            autoclick();
+            delay = delay - multi;
+            }
+        }
+        
+        
+    });
+        document.getElementById("clicks").innerHTML = "Clicks: " + numclicks;
+        document.getElementById("auto").innerHTML = "Autoclickers: " + clicks;
+        document.getElementById("delay").innerHTML = "Delay: " + delay + "ms";
+    },10);
+    
+}
+
+window.onload = gameLoop;
