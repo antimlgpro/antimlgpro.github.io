a5c3302cb1277be250fba9854adf5f7c8d666dc1
antimlgpro <ludvigvonreis@gmail.com>
2018-11-07 13:19:32 +0100

    Update test.js

--- test.js
+++ test.js
@@ -54,7 +54,7 @@ function sleep(delay2) {
       }
 
 function gameLoop() {
-    console.log("version: 2.0.14");
+    console.log("version: 2.0.15");
     loading();
     window.setInterval(function(){
     items.forEach(function(value){
