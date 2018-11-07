2ab41a6283100a16a319bd286ba2a6fd48c39051
antimlgpro <ludvigvonreis@gmail.com>
2018-11-07 13:33:22 +0100

    Update test.js

--- test.js
+++ test.js
@@ -72,7 +72,7 @@ function gameLoop() {
         if(delay <= delayMax) {
             delay = 0;
         }
-        numclicks.toFixed(0);
+        numclicks.parseFloat(numclicks).toFixed(0);
         document.getElementById("clicks").innerHTML = "Clicks: " + numclicks;
         document.getElementById("auto").innerHTML = "Autoclickers: " + clicks;
         document.getElementById("delay").innerHTML = "Delay: " + delay + "ms";
