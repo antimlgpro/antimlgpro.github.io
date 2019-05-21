0391cc4de5f58f317178276ad7f874b3366fcfdb
antimlgpro <ludvigvonreis@gmail.com>
2019-05-21 12:20:55 +0200

    Add files via upload

--- clicker.js
+++ clicker.js
@@ -10,9 +10,10 @@ var items = [];
 
 var autoClicker;
 var betterClicker;
+var farmClicker;
 
 // version
-var version = "2.1.0";
+var version = "2.1.1";
 
 // debug
 var debug = false;
@@ -22,12 +23,14 @@ function startGame()
     game.start();
     
     // items
-    autoClicker = new createClicker("autoclicker", 15, 15, 0, 1, 5000);
-    betterClicker = new createClicker("betterclicker", 50, 20, 0, 2, 2500)
+    autoClicker = new createClicker("autoClicker", 15, 15, 0, 1, 5000);
+    betterClicker = new createClicker("betterClicker", 50, 20, 0, 2, 2500);
+    farmClicker = new createClicker("farmClicker", 200, 25, 0, 5, 5500);
     
     //intervals
     this.autoLoop = setInterval(autoClicker.update, autoClicker.delay);
     this.betterLoop = setInterval(betterClicker.update, betterClicker.delay);
+    this.farmLoop = setInterval(farmClicker.update, farmClicker.delay);
     
     // info
     console.log("LOADED \nVersion: " + version);
@@ -50,11 +53,15 @@ function update()
 function addClick(amount,name) 
 {
     clicks = clicks + amount;
+    
     if(debug == true){console.log("addclick." + name + "\n" + amount);}
-    document.getElementById("clicksPlus").innerHTML = "+" + amount;
-    setTimeout(function(){ 
-    document.getElementById("clicksPlus").innerHTML = "";
-    }, 1000);
+    
+    if(amount != 0) {
+        document.getElementById("clicksPlus").innerHTML = "+" + amount;
+        setTimeout(function(){ 
+        document.getElementById("clicksPlus").innerHTML = "";
+        }, 1000);
+    }
 }
 
 function createClicker(name, cost, multi, cps, cpsConst, delay) 
@@ -114,14 +121,39 @@ function updateTxt()
     document.getElementById("clicks").innerHTML = "Clicks: " + clicks;
     
     // autoclicker
-    document.getElementById("auto").innerHTML = "Autoclickers: " + items.filter(function(x){return x=="autoclicker"}).length;
-    document.getElementById("delay").innerHTML = "Delay: " + autoClicker.delay + "ms " + autoClicker.delay/1000 + "s";
-    document.getElementById("cost").innerHTML = "Cost: " + autoClicker.cost;
+    document.getElementById("auto").innerHTML = "Autoclickers: " + items.filter(function(x){return x=="autoClicker"}).length;
+    document.getElementById("delay0").innerHTML = "Delay: " + autoClicker.delay + "ms " + autoClicker.delay/1000 + "s";
+    document.getElementById("cost0").innerHTML = "Cost: " + autoClicker.cost;
     
     // betterclicker
-    document.getElementById("better").innerHTML = "Betterclickers: " + items.filter(function(x){return x=="betterclicker"}).length;
-    document.getElementById("delay2").innerHTML = "Delay: " + betterClicker.delay + "ms " + betterClicker.delay/1000 + "s";
-    document.getElementById("cost2").innerHTML = "Cost: " + betterClicker.cost;
+    document.getElementById("better").innerHTML = "Betterclickers: " + items.filter(function(x){return x=="betterClicker"}).length;
+    document.getElementById("delay1").innerHTML = "Delay: " + betterClicker.delay + "ms " + betterClicker.delay/1000 + "s";
+    document.getElementById("cost1").innerHTML = "Cost: " + betterClicker.cost;
+    // farmclicker
+    document.getElementById("farm").innerHTML = "Farmclickers: " + items.filter(function(x){return x=="farmClicker"}).length;
+    document.getElementById("delay2").innerHTML = "Delay: " + farmClicker.delay + "ms " + farmClicker.delay/1000 + "s";
+    document.getElementById("cost2").innerHTML = "Cost: " + farmClicker.cost;
 }
 
 window.onload = startGame();
+
+
+
+
+const clickerItems = [
+    {name: autoClicker.name, delay: autoClicker.delay, cost: autoClicker.cost, costConst: '0', delayConst: '0'},
+    {name: betterClicker.name, delay: betterClicker.delay, cost: betterClicker.cost, costConst: '1', delayConst: '1'},
+    {name: farmClicker.name, delay: farmClicker.delay, cost: farmClicker.cost, costConst: '2', delayConst: '2'}
+];
+
+const markup = `
+    ${clickerItems.map(item => `
+    <div class="clickDiv">
+        <button onclick="buyItem(${item.name})" class="clickbtn">Buy ${item.name}</button>
+        <p id="delay${item.delayConst++}">Delay: ${item.delay}</p>
+        <p id="cost${item.costConst++}">Cost: ${item.cost}</p>
+     </div>
+    `).join('')}
+`;
+
+document.getElementById("clickerDiv").innerHTML = markup;
