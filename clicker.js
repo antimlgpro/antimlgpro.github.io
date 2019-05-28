c1ef32b773ac0b1d8d5381568feae31fafa994fd
antimlgpro <ludvigvonreis@gmail.com>
2019-05-28 14:39:05 +0200

    Add files via upload

--- clicker.js
+++ clicker.js
@@ -15,58 +15,55 @@ var version = "2.3.0";
 // debug
 var debug = false;
 
-function startGame() 
-{
+function startGame() {
     game.start();
-    
+
     // Items (name, cost, multi, cps, cpsConst, delay)
-    clickers.push(new createClicker("Clicker1",   15,  15, 0, 1, 5000));
-    clickers.push(new createClicker("Clicker2", 50,  20, 0, 2, 2500));
-    clickers.push(new createClicker("Clicker3",   200, 25, 0, 5, 5500));
-    clickers.push(new createClicker("Clicker4",500, 25, 0, 10, 6000));
-    clickers.push(new createClicker("Clicker5",1000, 25, 0, 20, 6500));
-    
-    
+    clickers.push(new createClicker("Clicker1", 15, 15, 0, 1, 5000));
+    clickers.push(new createClicker("Clicker2", 50, 20, 0, 2, 2500));
+    clickers.push(new createClicker("Clicker3", 200, 25, 0, 5, 5500));
+    clickers.push(new createClicker("Clicker4", 500, 25, 0, 10, 6000));
+    clickers.push(new createClicker("Clicker5", 1000, 25, 0, 20, 6500));
+
+
     // Intervals
-    for(var i = 0; i < clickers.length; i++) {
-    setInterval(clickers[i].update, clickers[i].delay);
-    
+    for (var i = 0; i < clickers.length; i++) {
+        setInterval(clickers[i].update, clickers[i].delay);
+
     }
     // Info
     console.log("LOADED \nVersion: " + version);
-    
+
 }
 
 var game = {
-    
-    start : function() 
-    {
+
+    start: function() {
         this.interval = setInterval(update, 10);
     }
-}
+};
 
-function update() 
-{   
+function update() {
     updateTxt();
 }
 
-function warning(text, time) 
-{
-    document.getElementById("warning").style.display = 'block';   
+function warning(text, time) {
+    document.getElementById("warning").style.display = 'block';
     document.getElementById("warning").innerHTML = text;
-    setTimeout(function(){ 
+    setTimeout(function() {
         document.getElementById("warning").style.display = 'none';
         document.getElementById("warning").innerHTML = "";
     }, time);
 }
 
-function updateTxt() 
-{
-    
+function updateTxt() {
+
     document.getElementById("clicks").textContent = "Clicks: " + clicks;
-    for(var i = 0; i < clickers.length; i++) {
-        document.getElementById("clicker" + i).textContent = clickers[i].name + ": " + items.filter(function(x){return x==clickers[i].name}).length;
-        document.getElementById("delay" + i).textContent = "Delay: " + (clickers[i].delay/1000).toFixed(0) + "s";
+    for (var i = 0; i < clickers.length; i++) {
+        document.getElementById("clicker" + i).textContent = clickers[i].name + ": " + items.filter(function(x) {
+            return x == clickers[i].name;
+        }).length;
+        document.getElementById("delay" + i).textContent = "Delay: " + (clickers[i].delay / 1000).toFixed(0) + "s";
         document.getElementById("cost" + i).textContent = "Cost: " + clickers[i].cost;
         document.getElementById("cps" + i).textContent = "Cps: " + clickers[i].cpsConst;
     }
@@ -74,65 +71,61 @@ function updateTxt()
 
 
 // Add click
-function addClick(amount,name) 
-{
+function addClick(amount, name) {
     clicks = clicks + amount;
-    
-    if(debug == true){console.log("addclick." + name + "\n" + amount);} // Debug
-    
-    if(amount != 0) {
+
+    if (debug === true) {
+        console.log("addclick." + name + "\n" + amount);
+    } // Debug
+
+    if (amount !== 0) {
         document.getElementById("plus").innerHTML = "+" + amount;
-        setTimeout(function(){ 
-        document.getElementById("plus").innerHTML = "";
+        setTimeout(function() {
+            document.getElementById("plus").innerHTML = "";
         }, 500);
     }
 }
 
 
 // Create clicker
-function createClicker(name, cost, multi, cps, cpsConst, delay) 
-{
+function createClicker(name, cost, multi, cps, cpsConst, delay) {
     this.name = name; // Name
     this.cost = cost; // Cost
     this.multi = multi; // Cost multiplier
     this.cps = cps; // Cps
     this.delay = delay; // Delay
     this.cpsConst = cpsConst;
-    
-    this.update = function() 
-    {
-        this.cps = cps; // Cps fix
-        if (items.length > 0) {
-            addClick(cps, name); // Click adding
-        }
-    },
-    this.changeCost = function() 
-    {
-        this.cost = (this.cost + (this.multi / 100) * this.cost).toFixed(0) * 1; // Cost increase
-        cps = cps + cpsConst; // Cps increase
-        if(debug == true){console.log("changeCost." + name + "\ncpsConst: " + cpsConst + "\ncps: " + cps);} // Debug
-        
-    }
+
+    this.update = function() {
+            this.cps = cps; // Cps fix
+            if (items.length > 0) {
+                addClick(cps, name); // Click adding
+            }
+        },
+        this.changeCost = function() {
+            this.cost = (this.cost + (this.multi / 100) * this.cost).toFixed(0) * 1; // Cost increase
+            cps = cps + cpsConst; // Cps increase
+            if (debug === true) {
+                console.log("changeCost." + name + "\ncpsConst: " + cpsConst + "\ncps: " + cps);
+            } // Debug
+
+        };
 }
 
 // Buying items
 
-function buyItem(item) 
-{
+function buyItem(item) {
     item = clickers[item];
     var cost = item.cost;
     var multi = item.multi;
     var name = item.name;
-    
-    if (clicks >= cost) 
-    {
+
+    if (clicks >= cost) {
         clicks = clicks - cost;
         items.push(name);
         item.changeCost();
-    } 
-    else 
-    {
-        warning("You need more clicks!", 1000)
+    } else {
+        warning("You need more clicks!", 1000);
     }
 }
 
@@ -141,8 +134,8 @@ window.onload = startGame();
 
 
 // Buy menu generation
-for(var i = 0; i < clickers.length; i++) {
-const markup = `
+for (var i = 0; i < clickers.length; i++) {
+    const markup = `
         <li class="mdc-list-item buy-list-item">
             <span class="mdc-list-item__text">
                 <span class="mdc-list-item__primary-text buy-list-text">${clickers[i].name}</span>
@@ -156,11 +149,10 @@ const markup = `
     </li>
 `;
 
-const markup2 = ` 
+    const markup2 = ` 
     <span class="value-text" id="clicker${i}">Clicker: </span>
 `;
 
-document.getElementById("buy-list").innerHTML += markup;
-document.getElementById("value-card-container").innerHTML += markup2;
+    document.getElementById("buy-list").innerHTML += markup;
+    document.getElementById("value-card-container").innerHTML += markup2;
 }
-
